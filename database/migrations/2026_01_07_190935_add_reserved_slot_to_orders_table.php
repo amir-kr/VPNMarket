4b5eba123b24956074b545a9f73a5cc33c1fa6c4
AMIR_KR <151685911+amir-kr@users.noreply.github.com>
2026-01-08 03:27:09 +0330

    Modify reserved_slot column addition in orders table

--- database/migrations/2026_01_07_190935_add_reserved_slot_to_orders_table.php
+++ database/migrations/2026_01_07_190935_add_reserved_slot_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->boolean('reserved_slot')->default(false)->after('server_id');
+            $table->boolean('reserved_slot')->default(false);
 
         });
     }
